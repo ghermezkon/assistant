2d6a88cfaada6129ad7609211b7db64cdaebbf91
ghermezkon@gmail.com <89772@bmi-khz.local>
2018-08-01 13:56:42 +0430

    970510

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+
+@Component({
+  selector: 'tabs-page',
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  constructor() {
+
+  }
+}
